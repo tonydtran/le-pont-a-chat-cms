3f69c35246888d631f6e1b993e27b1874603bc87
Anthony <anthony.tranvan@gmail.com>
2022-08-20 00:55:33 +0200

    Replace "path" property by "slug" and add lifecycles for Menu Item

--- src/api/product/content-types/product/lifecycles.js
+++ src/api/product/content-types/product/lifecycles.js
@@ -5,5 +5,6 @@ const { v4: uuid } = require('uuid');
 module.exports = {
   beforeCreate: async (data) => {
     data.params.data.uuid = uuid();
+    data.params.data.slug = `/${data.params.data.name.toLowerCase().replace(/ /g, '-')}`;
   },
 };
