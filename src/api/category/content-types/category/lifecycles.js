5a7c6a66f189e554b5c744d4165b13c37169063b
Anthony <anthony.tranvan@gmail.com>
2022-08-21 17:15:24 +0200

    Replace special characters for slugs

--- src/api/category/content-types/category/lifecycles.js
+++ src/api/category/content-types/category/lifecycles.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const { v4: uuid } = require('uuid');
+const replaceSpecialCharacters = require('replace-special-characters');
 
 module.exports = {
   beforeCreate: async (data) => {
     data.params.data.uuid = uuid();
-    data.params.data.slug = `/${data.params.data.name.toLowerCase().replace(/ /g, '-')}`;
+    data.params.data.slug = `/${replaceSpecialCharacters(data.params.data.name.toLowerCase().replace(/ /g, '-'))}`;
   },
   beforeUpdate: async (data) => {
     if (data.params.data.name) {
-      data.params.data.slug = `/${data.params.data.name.toLowerCase().replace(/ /g, '-')}`;
+      data.params.data.slug = `/${replaceSpecialCharacters(data.params.data.name.toLowerCase().replace(/ /g, '-'))}`;
     }
   }
 };
