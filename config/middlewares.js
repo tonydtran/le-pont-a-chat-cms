9f49d816cc8e2c6bd75d18e0fedfdec428eec98e
Anthony <anthony.tranvan@gmail.com>
2022-09-04 17:36:41 +0200

    Update middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,32 +1,44 @@
-module.exports = [
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:', 'http:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'http:'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'http:'],
-          upgradeInsecureRequests: null,
+module.exports = ({ env }) => {
+  let connectSrc = ["'self'", 'https:']
+  let imgSrc = ["'self'", 'data:', 'blob:', 'res.cloudinary.com']
+  let mediatSrc = ["'self'", 'data:', 'blob:', 'res.cloudinary.com']
+
+  if (env('NODE_ENV') === 'development') {
+    connectSrc = ["'self'", 'https:', 'http:']
+    imgSrc = ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'http:']
+    mediatSrc = ["'self'", 'data:', 'blob:', 'res.cloudinary.com', 'http:']
+  }
+
+  return [
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': connectSrc,
+            'img-src': imgSrc,
+            'media-src': mediatSrc,
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  'strapi::errors',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-  'strapi::body',
-  {
-    name: 'strapi::compression',
-    config: {
-      br: false,
-      gzip: true
+    'strapi::errors',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+    'strapi::body',
+    {
+      name: 'strapi::compression',
+      config: {
+        br: false,
+        gzip: true
+      },
     },
-  },
-];
+  ]
+}
