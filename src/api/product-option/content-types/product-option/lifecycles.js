6b4fc354a68ac79bd3c2f32951479f3b66486e50
Anthony <anthony.tranvan@gmail.com>
2022-09-14 12:46:19 +0200

    Add input_type default value after create/update

--- src/api/product-option/content-types/product-option/lifecycles.js
+++ src/api/product-option/content-types/product-option/lifecycles.js
@@ -5,5 +5,18 @@ const { v4: uuid } = require('uuid');
 module.exports = {
   beforeCreate: async (data) => {
     data.params.data.uuid = uuid();
+
+    if (!data.params.data.input_type) {
+      data.params.data.option_values.length > 4
+        ? data.params.data.input_type = 'select'
+        : data.params.data.input_type = 'radio'
+    }
+  },
+  beforeUpdate: async (data) => {
+    if (!data.params.data.input_type) {
+      data.params.data.option_values.length > 4
+        ? data.params.data.input_type = 'select'
+        : data.params.data.input_type = 'radio'
+    }
   },
 };
