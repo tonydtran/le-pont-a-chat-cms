49108db922c3c5af1892a928a774c6edd0329be0
Anthony <anthony.tranvan@gmail.com>
2023-02-26 15:56:31 +0100

    Fix build part. 2

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,17 @@
 module.exports = ({ env }) => ({
   'duplicate-button': true,
+  'import-export-entries': {
+    enabled: true,
+    config: {
+      /**
+       * Public hostname of the server.
+       *
+       * If you use the local provider to persist medias,
+       * `serverPublicHostname` should be set to properly export media urls.
+       */
+      serverPublicHostname: '', // default: "".
+    },
+  },
   'transformer': {
     enabled: true,
     config: {
